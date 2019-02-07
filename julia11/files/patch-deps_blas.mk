272a0e1929f0b7368f545de6d4082fa511371d66
Iblis Lin <iblis@hs.ntnu.edu.tw>
2019-02-07 19:22:02 +0800

    julia11: private openblas

--- julia11/files/patch-deps_blas.mk
+++ julia11/files/patch-deps_blas.mk
@@ -1,11 +1,17 @@
---- deps/blas.mk.orig	2019-02-06 00:40:20.032286000 +0800
-+++ deps/blas.mk	2019-02-06 00:44:36.622010000 +0800
-@@ -95,6 +95,8 @@
+--- deps/blas.mk.orig	2019-01-13 00:25:11 UTC
++++ deps/blas.mk
+@@ -95,6 +95,14 @@ OPENBLAS_BUILD_OPTS += MAKE_NB_JOBS=0
  ifneq ($(USE_BINARYBUILDER_OPENBLAS), 1)
  
  $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-configured: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/source-extracted
 +	cd $(BUILDDIR)/$(OPENBLAS_SRC_DIR) && \
 +		patch -p1 -f < $(SRCDIR)/patches/openblas-fix-arch.patch
++	/usr/bin/sed -i.bak -e "s+(ARCH)+(AR)+" \
++		$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/lapack-netlib/SRC/Makefile \
++		$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/lapack-netlib/SRC/VARIANTS/Makefile \
++		$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/lapack-netlib/TESTING/MATGEN/Makefile \
++		$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/lapack-netlib/LAPACKE/src/Makefile \
++		$(BUILDDIR)/$(OPENBLAS_SRC_DIR)/lapack-netlib/LAPACKE/utils/Makefile
  	echo 1 > $@
  
  $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-compiled: $(BUILDDIR)/$(OPENBLAS_SRC_DIR)/build-configured
